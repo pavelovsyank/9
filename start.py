2b760fd60ac029eeed263bb0889cd41453c64eb2
pavel <p@b.ru>
2024-02-08 01:34:31 +0200

    port from config

--- start.py
+++ start.py
@@ -3,4 +3,4 @@ from app.config import settings
 
 if __name__ == "__main__":
     #print(f"port = {settings.port}")
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8003, log_level="info")
+    uvicorn.run("app.main:app", host="0.0.0.0", port=settings.port, log_level="info")
